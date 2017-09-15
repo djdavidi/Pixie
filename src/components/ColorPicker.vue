debf4f058a96c5399c8f44b875fd18477a66c24e
[David Scherban] <davidscherban@gmail.com>
2017-09-15 02:17:36 -0400

    color picker working - need to connect to parent and style abit further

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="color-picker">
-	<input type="range" @change="calculateColor" v-model="hue">
-	<input type="range"  @change="calculateColor" v-model="saturation">
-	<input type="range"  @change="calculateColor" v-model="lightness">
-	<div :style="{background: currentColor}" class="current-color">&nbsp;</div>
+  	<!-- <div class="range-container"> -->
+		<input type="range" @change="calculateColor" v-model="hue">
+		<input type="range"  @change="calculateColor" v-model="saturation">
+		<input type="range"  @change="calculateColor" v-model="lightness">
+	<!-- </div> -->
+	<div :style="{background: calculateColor()}" class="current-color">&nbsp;</div>
   	<div class="color-patch-container">
-		<div v-for="n in 12" class="color-patch" :style="{background: 'hsl('+ n*30 +', 100%, 50%)'}">
+		<div v-for="n in 12"  @click="setColor(n*30)" class="color-patch" :style="{background: 'hsl('+ n*30 +', 100%, 50%)'}">
 			&nbsp;
 		</div>
 	</div>
-	<!-- YO {{calculateColor}} OY -->
   </div>
 </template>
 
@@ -18,10 +19,10 @@ export default {
   name: 'colorpicker',
   data () {
     return {
-      hue: 1,
-      saturation: 1,
-      lightness: 1,
-      currentColor: this.calculateColor
+      hue: 50,
+      saturation: 100,
+      lightness: 50
+      // currentColor: this.calculateColor
     }
   },
   methods: {
@@ -31,7 +32,7 @@ export default {
   		return `hsl(${this.hue * 3.6}, ${this.saturation}%, ${this.lightness}%)`;
   	},
   	setColor(hue) {
-  		this.hue = hue;
+  		this.hue = hue/3.6;
   		this.saturation = 100;
   		this.lightness = 50;
   		this.calculateColor();
@@ -46,7 +47,9 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .color-picker {
-	width: 100%;
+	width: 50%;
+	display: flex;
+	flex-direction: column;
 }
 
 .current-color {
@@ -57,6 +60,10 @@ export default {
 	display: grid;
 	width: 50%;
 	grid-template-columns: 1fr  1fr;
+}
+
+input {
+	width: 50%;
 }
 
 .color-patch {
